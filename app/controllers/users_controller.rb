9239755dd88e0a082111f9249814ebe0cc580a3d
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-11 17:26:44 -0500

    Fix issues with rubocop linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
-  def index
-  end
-  
+  def index; end
+
   def new
     @user = User.new
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
@@ -19,7 +18,7 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   def update
     @user = User.find(params[:id])
 
@@ -35,6 +34,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
