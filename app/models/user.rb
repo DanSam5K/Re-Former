1bbc466856f4a80afe90927c3e1b445b650fc600
Daniel Samuel <dansam5000@gmail.com>
2021-06-11 02:34:51 +0100

    Add validation to each user model field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6, maximum: 16 }
 end
