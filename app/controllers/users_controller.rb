451c9ab948c39c48d78ebef028880724ad859044
Daniel Samuel <dansam5000@gmail.com>
2021-06-11 22:43:07 +0100

    Add edit html amd fix some bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 
   def create
     #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
@@ -13,6 +17,16 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
